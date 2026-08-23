fb0245815214f0e98153701ef71678544bbc25c9
Akshith <akshiththota30@gmail.com>
2026-08-23 19:25:08 +0530

    feat: add user, movie, and theater CRUD flows
    Add entities, repositories, DTOs, mappers, services, and controllers for User, Movie, and Theater.

---
+++ src/main/java/com/Akshith/movie_reservation_system/dto/auth/AuthRequestDto.java
@@ -0,0 +1,4 @@
+package com.Akshith.movie_reservation_system.dto.auth;
+
+public class AuthRequestDto {
+}
---
+++ src/main/java/com/Akshith/movie_reservation_system/dto/reservation/ReservationResponseDto.java
@@ -0,0 +1,4 @@
+package com.Akshith.movie_reservation_system.dto.reservation;
+
+public class ReservationResponseDto {
+}
